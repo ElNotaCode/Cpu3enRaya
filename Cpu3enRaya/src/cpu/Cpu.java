84a55727b2c67cbe090c4ce9306158cf9da2fa4f
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-08 12:30:02 +0200

    comentarios

---
+++ Cpu3enRaya/src/cpu/Cpu.java
@@ -0,0 +1,28 @@
+package cpu;
+
+public class Cpu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//imprimir array
+	
+	//tirada jugador
+	
+	//turno
+	
+	//metodo comprobar
+	
+	//metodo comprobar victoria
+	
+	//primera tirada
+	
+	//tirada turno 2
+	
+	//tirada turno 3
+	
+	//generador de estrategias
+
+}
