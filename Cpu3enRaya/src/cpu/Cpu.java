4e9d2f895fd0132eab78809636431c5ad8b43828
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-08 20:26:08 +0200

    cpu fase 1 finalizada

--- Cpu3enRaya/src/cpu/Cpu.java
+++ Cpu3enRaya/src/cpu/Cpu.java
@@ -81,22 +81,14 @@ public class Cpu {
 			//pasamos turno
 			jugador = false;
 		}else {
-			//comportamiento de la cpu segun el turno
-			//turno 1 o 2 coloca random
-			if(numeroTurno == 1 || numeroTurno == 2) {
-				primeraTirada();
-			}
-			//turno 3 intenta jugada
-			if(numeroTurno == 3 || numeroTurno == 4) {
-				segundaTirada();
-			}
-			//turno 4 intenta bloquear
-			
+			//tira la cpu con su estrategia
+			comportamientoCpu();
 			//se pasa el turno
 			jugador = true;
 		}
 		
 	}
+	
 	//metodo comprobar
 	public static boolean comprobarFicha(int i, int j) {
 		
@@ -114,8 +106,20 @@ public class Cpu {
 		}
 	}
 	
+	public static void comportamientoCpu() {
+		//comportamiento de la cpu segun el turno
+		//turno 1 o 2 coloca random
+		if(numeroTurno == 1 || numeroTurno == 2) {
+			primeraTiradaCpu();
+		}else if(numeroTurno == 3) { //turno 3 intenta jugada
+			segundaTiradaCpu();
+		}else{
+			demasTiradasCpu(); //turno 4 o mas intenta bloquear
+		}
+	}
+	
 	//primera tirada (turno 1 o 2)
-	public static void primeraTirada() {
+	public static void primeraTiradaCpu() {
 		int i;
 		int j;
 		do {
@@ -124,7 +128,160 @@ public class Cpu {
 		}while(!comprobarFicha(i,j));
 		tablero[i][j] = 'O';
 	}
+
+	//tirada turno 3 intenta seguir jugada o tirada turno 4 intenta bloquear
+	public static void segundaTiradaCpu() {
+		//si es el turno 3 recorre el array y intenta poner una ficha al lado
+			for (int i = 0; i < tablero.length; i++) {
+				for (int j = 0; j < tablero[i].length; j++) {
+					//detectamos la ficha
+					if(tablero[i][j] == 'O') {
+						colocarAlrededor(i,j);
+						
+					}
+				}
+			}
+	}
 	
+	public static void demasTiradasCpu() {
+		//si es el turno 4 o mas va a bloquear
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				//detectamos las fichas a bloquear
+				if(tablero[i][j] == 'X') {
+				//llamamos a un metodo que calcule donde falta para que se cumpla la condición de victoria y bloquee
+				bloquearJugada(i, j);
+				break;
+				}
+			}
+		}
+	}
+	
+	//generador de estrategias
+	
+	//metodo para bloquear jugada
+	public static void bloquearJugada(int i, int j) {
+		//miramos al rededor de la posición y buscamos la otra para bloquear el 3 en raya
+		/*	    j0    j1   j2
+		 * i0 [0 0] [0 1] [0 2]
+		 * i1 [1 0] [1 1] [1 2]
+		 * i2 [2 0] [1 2] [2 2]
+		 */
+		int ii = i;
+		int jj = j;
+		
+		//top and block down
+		--ii;
+		if(comprobarFicha(ii, jj)) {
+			if(tablero[ii][jj] == 'X') {
+				//si coincide miramos de colocarla en la parte contraria
+				if(comprobarFicha(ii+2, jj)){
+					tablero[ii+2][jj] = 'O';
+					return;
+				}
+			}
+		}
+		
+		//top left block bot right
+		--jj;
+		if(comprobarFicha(ii, jj)) {
+			if(tablero[ii][jj] == 'X') {
+				//si coincide miramos de colocarla en la parte contraria
+				if(comprobarFicha(ii-2, jj-2)){
+					tablero[ii+2][jj+2] = 'O';
+					return;
+				}
+			}
+		}
+		
+		//top right to block bot left
+		jj =+2;
+		if(comprobarFicha(ii, jj)) {
+			if(tablero[ii][jj] == 'X') {
+				//si coincide miramos de colocarla en la parte contraria
+				if(comprobarFicha(ii+2, jj-2)){
+					tablero[ii+2][jj+2] = 'O';
+					return;
+				}
+			}
+		}
+		
+		//left to right
+		ii= i;
+		jj = j-1;
+		if(comprobarFicha(ii, jj)) {
+			if(tablero[ii][jj] == 'X') {
+				//si coincide miramos de colocarla en la parte contraria
+				if(comprobarFicha(ii, jj+2)){
+					tablero[ii][jj+2] = 'O';
+					return;
+				}
+			}
+		}
+		//right to left
+		jj = j+1;
+		if(comprobarFicha(ii, jj)) {
+			if(comprobarFicha(ii, jj)) {
+				if(tablero[ii][jj] == 'X') {
+					//si coincide miramos de colocarla en la parte contraria
+					if(comprobarFicha(ii, jj-2)){
+						tablero[ii][jj-2] = 'O';
+						return;
+					}
+				}
+			}
+		}
+		//down to top
+		--ii;
+		jj = j;
+		if(comprobarFicha(ii, jj)) {
+			if(comprobarFicha(ii, jj)) {
+				if(tablero[ii][jj] == 'X') {
+					//si coincide miramos de colocarla en la parte contraria
+					if(comprobarFicha(ii+2, jj)){
+						tablero[ii+2][jj] = 'O';
+						return;
+					}
+				}
+			}
+		}
+		
+		//down left to top right
+		jj--;
+		if(comprobarFicha(ii, jj)) {
+			if(comprobarFicha(ii, jj)) {
+				if(tablero[ii][jj] == 'X') {
+					//si coincide miramos de colocarla en la parte contraria
+					if(comprobarFicha(ii+2, jj+2)){
+						tablero[ii+2][jj] = 'O';
+						return;
+					}
+				}
+			}
+		}
+		
+		//down right
+		jj += 2;
+		if(comprobarFicha(ii, jj)) {
+			if(comprobarFicha(ii, jj)) {
+				if(tablero[ii][jj] == 'X') {
+					//si coincide miramos de colocarla en la parte contraria
+					if(comprobarFicha(ii+2, jj-2)){
+						tablero[ii+2][jj] = 'O';
+						return;
+					}
+				}
+			}
+		}
+		
+		//si no va a intentar colocar una para si misma
+		colocarAlrededor(ii, jj);
+		
+		//TODO: mirar forma de randomizar el patrón
+		
+	}
+	
+	//metodo que mira alrededor de la ficha y intenta colocar otra
 	public static void colocarAlrededor(int i, int j) {
 		//miramos al rededor de la posición
 		/*	    j0    j1   j2
@@ -132,7 +289,6 @@ public class Cpu {
 		 * i1 [1 0] [1 1] [1 2]
 		 * i2 [2 0] [1 2] [2 2]
 		 */
-		//!comprobarFicha(i, j) && i<0 && i>2 && j<0 && j>2
 		int ii = i;
 		int jj = j;
 		
@@ -140,18 +296,21 @@ public class Cpu {
 		--ii;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
 		//top right
 		--jj;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
 		//top left
 		jj =+2;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
 		//left
@@ -159,50 +318,39 @@ public class Cpu {
 		jj = j-1;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		//right
 		jj = j+1;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		//down
 		--ii;
 		jj = j;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
 		//down left
 		jj--;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
 		//down right
 		jj += 2;
 		if(comprobarFicha(ii, jj)) {
 			tablero[ii][jj] = 'O';
+			return;
 		}
 		
-	}
-	
-	//tirada turno 3 intenta seguir jugada o tirada turno 4 intenta bloquear
-	public static void segundaTirada() {
-		//si es el turno 3 recorre el array y intenta poner una ficha al lado
-		
-		for (int i = 0; i < tablero.length; i++) {
-			for (int j = 0; j < tablero[i].length; j++) {
-				//detectamos la ficha
-				if(tablero[i][j] == 'O') {
-					colocarAlrededor(i,j);
-					
-				}
-			}
-		}
+		//TODO: mirar forma de randomizar el patrón
 		
 	}
-	
-	//generador de estrategias
 	
 	//metodo comprobar victoria
 	public static boolean comprobarVictoria() {
